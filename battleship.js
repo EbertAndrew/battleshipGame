7cf0cf4fab8997f4e74ef5b407f710bcfb590f59
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-06 14:42:01 -0500

    Created base game

---
+++ battleship.js
@@ -0,0 +1,36 @@
+// All variables
+var randomLoc = Math.floor(Math.random()    * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+// Loop for game logic
+while (isSunk == false) {
+
+    // Obtain user's guess.
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+
+    // Check guess for validity. || is an "or" operator.
+    if(guess < 0 || guess > 6){
+        alert("Please enter a valid number!!");
+    }else{
+        guesses = guesses + 1;
+        // Check for a hit
+        if(guess == location1 || guess == location2 || guess == location3){
+            alert("HIT!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }   else {
+            alert("!MISSED!");
+        }
+    }
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
